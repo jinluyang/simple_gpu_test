03b45f089ae9970ab13858fb2cca0eb45cfe3883
jinluyang <1055057679@qq.com>
2022-10-18 16:11:36 +0800

    Update malloc_test.cpp

--- malloc_test.cpp
+++ malloc_test.cpp
@@ -5,9 +5,10 @@
 int main() {
 float* p = NULL;
 //size_t size = 1 << 20; // 1MB
-size_t size = 1 << 30 // 1GB
+size_t size = 1UL << 30 // 1GB
 cudaError_t err = cudaSuccess;
 err = cudaMalloc((void**)&p, size);
 std::cout << cudaGetErrorString(err) << std::endl;
+cudaFree(p);
 return 0;
 }
